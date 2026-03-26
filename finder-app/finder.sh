dfe6c1e60e4ed469104303b8815ee4d85d2410ef
zhya2609 <268274666+zhya2609@users.noreply.github.com>
2026-03-25 23:50:01 -0700

    assignment-3-part-1 commit

---
+++ finder-app/finder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 path_to_filesdir"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Usage: $0 path_to_searchstr"
+    exit 1
+fi
+
+count_files=$(find "$1" -type f | wc -l)
+count_lines=$(find "$1" -type f -exec grep -H "$2" {} \; | wc -l)
+
+echo "The number of files are $count_files and the number of matching lines are $count_lines"
