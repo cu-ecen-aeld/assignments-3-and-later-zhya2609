cd3f546401568fbcdda49a13e072e5bf966071af
zhya2609 <268274666+zhya2609@users.noreply.github.com>
2026-04-18 15:19:15 -0700

    Assignment5 part2

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+case "$1" in
+  start)
+    echo "Starting aesdsocket"
+    start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+    ;;
+  stop)
+    echo "Stopping aesdsocket"
+    start-stop-daemon -K -n aesdsocket -s SIGTERM
+    ;;
+  *)
+    echo "Usage: $0 {start|stop}"
+    exit 1
+esac
+exit 0
