814180598f2a199735e5fe0bc12d1c2c4282e778
zhya2609 <268274666+zhya2609@users.noreply.github.com>
2026-04-18 21:23:22 -0700

    fix daemon arg

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -3,7 +3,8 @@
 case "$1" in
   start)
     echo "Starting aesdsocket"
-    start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+    # start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+    start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket
     ;;
   stop)
     echo "Stopping aesdsocket"
